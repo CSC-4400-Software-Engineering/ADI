fa5b21acdf3193295ae3c605fa34936b458f585d
sgoyett2 <77983374+sgoyett2@users.noreply.github.com>
2021-03-03 11:32:26 -0500

    Created SQL file
    This is the first draft of the database in a SQl file. I'm not sure if I even uploaded this correctly.

---
+++ apexdesignandinterfaces.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2021 at 05:29 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apexdesignandinterfaces`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `onlineorder`
+--
+
+CREATE TABLE `onlineorder` (
+  `orderID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `onlineorderproduct`
+--
+
+CREATE TABLE `onlineorderproduct` (
+  `OOPID` int(11) NOT NULL,
+  `orderID` int(11) NOT NULL,
+  `productID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `productID` int(11) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `price` int(5) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `picture` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `firstName` char(50) NOT NULL,
+  `lastName` char(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `streetAddress` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `phone` bigint(10) NOT NULL,
+  `securityQuestion` varchar(100) NOT NULL,
+  `securityAnswer` int(100) NOT NULL,
+  `userType` int(1) NOT NULL,
+  `creditCardNum` bigint(16) NOT NULL,
+  `creditCardExp` int(4) NOT NULL,
+  `creditCardCVV` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `onlineorder`
+--
+ALTER TABLE `onlineorder`
+  ADD PRIMARY KEY (`orderID`),
+  ADD UNIQUE KEY `ID` (`orderID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `onlineorderproduct`
+--
+ALTER TABLE `onlineorderproduct`
+  ADD PRIMARY KEY (`OOPID`),
+  ADD KEY `orderID` (`orderID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productID`),
+  ADD UNIQUE KEY `ID` (`productID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `ID` (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `onlineorder`
+--
+ALTER TABLE `onlineorder`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `onlineorderproduct`
+--
+ALTER TABLE `onlineorderproduct`
+  MODIFY `OOPID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `onlineorder`
+--
+ALTER TABLE `onlineorder`
+  ADD CONSTRAINT `onlineorder_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE NO ACTION;
+
+--
+-- Constraints for table `onlineorderproduct`
+--
+ALTER TABLE `onlineorderproduct`
+  ADD CONSTRAINT `onlineorderproduct_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `onlineorder` (`orderID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `onlineorderproduct_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
